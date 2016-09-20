c7d4e0d34b590d14aea5e2e21957fa8ea913464d
cho <cho@DESKTOP-088LLCO>
2016-09-20 20:28:20 +0900

    report end

--- src/prob4/CalcApp.java
+++ src/prob4/CalcApp.java
@@ -1,8 +1,37 @@
 package prob4;
 
+import java.util.*;
+
 public class CalcApp {
 
 	public static void main(String[] args) {
-		// Scanner 와  String 클래스의 split 함수를 사용합니다.
+		// Scanner 와 String 클래스의 split 함수를 사용합니다.
+		Scanner scanner = new Scanner(System.in);
+
+		while(true) {
+			System.out.print(">> ");
+			String str = scanner.nextLine();
+			
+			if(str.equals("quit")) {
+				break;
+			}
+			
+			String[] arr = str.split(" ");
+			int a = Integer.parseInt(arr[0]);
+			int b = Integer.parseInt(arr[2]);
+
+			if (arr[1].equals("+")) {
+				System.out.println(a + b);
+			} else if (arr[1].equals("-")) {
+				System.out.println(a - b);
+			} else if (arr[1].equals("*")) {
+				System.out.println(a * b);
+			} else {
+				System.out.println(a / b);
+			}
+			
+			
+		}
+		
 	}
 }
